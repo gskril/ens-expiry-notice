05bad0f27d8cd7efa88428b87f0c7ec180f8ca8d
Greg <gskril@gmail.com>
2023-04-15 15:56:30 +0900

    Remove unused code

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,36 +1,4 @@
-import { OnCronjobHandler, OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
-
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
-  switch (request.method) {
-    case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
-  }
-};
+import { OnCronjobHandler } from '@metamask/snaps-types';
 
 export const onCronjob: OnCronjobHandler = async ({ request }) => {
   const name = 'gregskril.eth';
@@ -42,13 +10,6 @@ export const onCronjob: OnCronjobHandler = async ({ request }) => {
 
       console.log(message);
       return notify(message);
-      // return snap.request({
-      //   method: 'snap_notify',
-      //   params: {
-      //     type: 'inApp',
-      //     message: `A name of yours expires on ${expiration}`,
-      //   },
-      // });
     }
 
     default:
