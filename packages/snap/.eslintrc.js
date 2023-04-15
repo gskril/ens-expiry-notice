0675a6d59f6bad57450e926493c78e24d02fbed7
Greg <gskril@gmail.com>
2023-04-15 21:45:58 +0900

    fix lint

--- packages/snap/.eslintrc.js
+++ packages/snap/.eslintrc.js
@@ -1,5 +1,46 @@
 module.exports = {
-  extends: ['../../.eslintrc.js'],
+  root: true,
+  parserOptions: {
+    sourceType: 'module',
+  },
 
-  ignorePatterns: ['!.eslintrc.js', 'dist/'],
+  extends: ['@metamask/eslint-config'],
+
+  overrides: [
+    {
+      files: ['**/*.js'],
+      extends: ['@metamask/eslint-config-nodejs'],
+    },
+
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: ['@metamask/eslint-config-typescript'],
+      rules: {
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      },
+    },
+
+    {
+      files: ['**/*.test.ts', '**/*.test.js'],
+      extends: ['@metamask/eslint-config-jest'],
+      rules: {
+        '@typescript-eslint/no-shadow': [
+          'error',
+          { allow: ['describe', 'expect', 'it'] },
+        ],
+      },
+    },
+  ],
+
+  ignorePatterns: [
+    '!.prettierrc.js',
+    '**/!.eslintrc.js',
+    '**/dist*/',
+    '**/*__GENERATED__*',
+    '**/build',
+    '**/public',
+    '**/.cache',
+    '!.eslintrc.js',
+    'dist/',
+  ],
 };
